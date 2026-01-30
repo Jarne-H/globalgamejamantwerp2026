e70d51bfc1faffe249902fd20f0d20ebe7cdbc8c
Jarne_H <horemans.jarne@gmail.com>
2026-01-30 23:10:02 +0100

    health adjust

--- GGJ2026/Assets/Scripts/PlayerMovement.cs
+++ GGJ2026/Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    private float _movementSpeed = 5.0f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Initialization code here
+
     }
 
     // Update is called once per frame
