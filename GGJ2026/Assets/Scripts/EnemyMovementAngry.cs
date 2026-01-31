aa5b097b56c41b4c3712a2028c1e6f66a573af56
WoutWenseleers <wout.wenseleers@student.howest.be>
2026-01-31 14:40:04 +0100

    angry fix + calm enemy

--- GGJ2026/Assets/Scripts/EnemyMovementAngry.cs
+++ GGJ2026/Assets/Scripts/EnemyMovementAngry.cs
@@ -5,17 +5,28 @@ public class EnemyMovementAngry : EnemyMovement
 {
     [SerializeField]
     private float _movementSpeed = 2.0f;
+
+    [Header("ChargeProperties")]
     [SerializeField]
     private float _chargeSpeed = 7.0f;
     [SerializeField]
-    private float _chargeCooldown = 2.0f;
+    private float _chargeCooldown = 5.0f;
     private float _timeSinceLastCharge = 0f;
     [SerializeField]
     private float _chargeAdditionalDistance = 5.0f;
     [SerializeField]
+    private float _chargeRadius = 8.0f;
+
+    [Header("ChargeSmoothing")]
+    [SerializeField]
     private float _chargeAccelerationTime = 1.0f;
     private float _timeSinceChargeStart = 0f;
+    [SerializeField]
+    private float _chargeDecelerationTime = 1.0f;
     private float _timeSinceChargeEnd = 0f;
+    //[SerializeField]
+    //private float _chargeEndIdleTime = 0.2f;
+    //private float _chargeIdleTime = 0f;
 
 
     private Vector3 _chargeTarget;
@@ -29,7 +40,12 @@ public class EnemyMovementAngry : EnemyMovement
     // Update is called once per frame
     void Update()
     {
-        if( _isCharging )
+        //_chargeIdleTime += Time.deltaTime;
+        //if (_chargeIdleTime < _chargeEndIdleTime)
+        //{
+        //    return;
+        //}
+        if (_isCharging)
         {
             _timeSinceChargeStart += Time.deltaTime;
             float chargeSpeed = _chargeSpeed;
@@ -37,34 +53,41 @@ public class EnemyMovementAngry : EnemyMovement
             {
                 chargeSpeed *= _timeSinceChargeStart / _chargeAccelerationTime;
             }
-            transform.position += (_chargeTarget - _chargeOriginalPosition).normalized * chargeSpeed * Time.deltaTime;
+
+            if(_timeSinceChargeEnd > 0)
+            {
+                chargeSpeed *= (_chargeDecelerationTime - _timeSinceChargeEnd) / _chargeDecelerationTime;
+            }
+            transform.position += (_chargeTarget - _chargeOriginalPosition).normalized * (chargeSpeed + _movementSpeed) * Time.deltaTime;
+
             Vector3 posToTargetDirection = (_chargeTarget - transform.position).normalized;
             Vector3 targetToOriginalPosDirection = (_chargeOriginalPosition - _chargeTarget).normalized;
-            if (Mathf.Abs(posToTargetDirection.x - targetToOriginalPosDirection.x) < 0.001 &&
-                Mathf.Abs(posToTargetDirection.y - targetToOriginalPosDirection.y) < 0.001)
+            if (Vector3.Angle(posToTargetDirection, targetToOriginalPosDirection) < 90)
             {
                 _timeSinceChargeEnd += Time.deltaTime;
-                if(_timeSinceChargeEnd > _chargeAccelerationTime)
+                if(_timeSinceChargeEnd > _chargeDecelerationTime)
                 {
                     _isCharging = false;
                     _timeSinceLastCharge = 0f;
                     _timeSinceChargeEnd = 0f;
-                    _timeSinceChargeStart = 0f;
+                    //_chargeIdleTime = 0;
                 }
-
             }
-        }
-        else if(_timeSinceLastCharge > _chargeCooldown)
-        {
-            Vector3 playerDirection = (PlayerTransform.position - transform.position).normalized;
-            _chargeTarget = PlayerTransform.position + playerDirection * _chargeAdditionalDistance;
-            _chargeOriginalPosition = transform.position;
-            _isCharging = true;
-            _timeSinceChargeStart = 0f;
         }
         else
         {
             _timeSinceLastCharge += Time.deltaTime;
+
+            transform.position += (PlayerTransform.position - transform.position).normalized * _movementSpeed * Time.deltaTime;
+
+            if((PlayerTransform.position - transform.position).magnitude < _chargeRadius && _timeSinceLastCharge > _chargeCooldown)
+            {
+                Vector3 playerDirection = (PlayerTransform.position - transform.position).normalized;
+                _chargeTarget = PlayerTransform.position + playerDirection * _chargeAdditionalDistance;
+                _chargeOriginalPosition = transform.position;
+                _isCharging = true;
+                _timeSinceChargeStart = 0f;
+            }
         }
 
     }
---
+++ GGJ2026/Assets/Scripts/EnemyMovementCalm.cs
@@ -0,0 +1,46 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class EnemyMovementCalm : EnemyMovement
+{
+    [SerializeField]
+    private float _movementSpeed = 5.0f;
+    [SerializeField]
+    private float _playerAngleClampSize_div_2 = 45f;
+    private float _movementDirectionAngle = 0f;
+
+    [SerializeField]
+    private float _angleTargetChangeSpeed = 90f;
+    [SerializeField]
+    private float _angleChangeSpeed = 0.5f;
+    private float _angleTarget = 0f;
+
+    [SerializeField]
+    private float _angleChangeCooldown = 2f;
+    private float _timeSinceLastAngleChange = 0f;
+
+    private void Start()
+    {
+        Vector3 playerDirection = (PlayerTransform.position - transform.position).normalized;
+        float playerAngle = Mathf.Rad2Deg * Mathf.Atan2(playerDirection.y, playerDirection.x);
+        _movementDirectionAngle = playerAngle;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerDirection = (PlayerTransform.position - transform.position).normalized;
+        float playerAngle = Mathf.Rad2Deg * Mathf.Atan2(playerDirection.y, playerDirection.x);
+
+        _timeSinceLastAngleChange += Time.deltaTime;
+        if(_timeSinceLastAngleChange > _angleChangeCooldown)
+        {
+            _timeSinceLastAngleChange -= _angleChangeCooldown;
+            _angleTarget = playerAngle + (Random.value - 0.5f) * _angleTargetChangeSpeed;
+        }
+
+        _movementDirectionAngle = Mathf.MoveTowardsAngle(_movementDirectionAngle, _angleTarget, _angleChangeSpeed);
+
+        Vector3 movementDirection = new Vector3(Mathf.Cos(Mathf.Deg2Rad * _movementDirectionAngle), Mathf.Sin(Mathf.Deg2Rad * _movementDirectionAngle), 0).normalized;
+        transform.position += movementDirection * _movementSpeed * Time.deltaTime;
+    }
+}
